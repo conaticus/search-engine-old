9b7fedc4fcaf4ecafddf3f7ad454d3d0ee51af83
conaticus <enquiry.conaticus@gmail.com>
2022-08-17 12:44:20 +0100

    add client side

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import router from "./routes/router";
 
 require("dotenv").config();
@@ -7,5 +8,8 @@ const server = express();
 server.use(express.json());
 
 server.use("/api", router);
+server.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/client/index.html"));
+});
 
 server.listen(process.env.PORT, () => console.log(`Listening at *:${process.env.PORT}`));
